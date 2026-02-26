681ce0961fa3acaa1a0d3f41b67c8b9ae2e01e50
Samir Khanal <gdssamir@gmail.com>
2026-02-26 13:21:46 +0545

    feat(api): remove logger import and related logging statement from server setup

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,6 +1,5 @@
 import { Hono } from "hono";
 import { env } from "./config/env.js";
-import { logger } from "./config/logger.js";
 import { corsMiddleware } from "./lib/cors.js";
 import { notFoundHandler, onErrorHandler } from "./lib/http-handlers.js";
 import { requestLogger } from "./middlewares/request-logger.js";
@@ -15,7 +14,6 @@ registerRoutes(app);
 
 app.notFound(notFoundHandler);
 app.onError(onErrorHandler);
-logger.info("API running", { port: env.PORT });
 
 export default {
   port: env.PORT,
